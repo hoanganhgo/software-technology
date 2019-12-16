de6f6b522b26be572bf597990c86cdaf50b8689d
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2019-12-16 20:18:51 +0700

    render các trang Bảng xếp hạng, Vua phá lưới, Tạo giải đấu mới, Quản lí giải đấu

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Trang chủ' });
 });
 router.get('/login',function(req,res,next){
-  res.render('login');
+  res.render('login', {title: 'Đăng nhập'});
 });
 router.get('/register',function(req,res,next){
-  res.render('register');
+  res.render('register', {title: 'Đăng ký'});
 })
+
+router.get('/ranking_table', function(req, res, next){
+  res.render('ranking_table', {title: 'Bảng xếp hạng'});
+});
+router.get('/goal_leader_list', function(req, res, next){
+  res.render('goal_leader_list', {title: 'Vua phá lưới'});
+});
+router.get('/tournament_create', function(req, res, next){
+  res.render('tournament_create', {title: 'Tạo giải đấu mới'});
+});
+router.get('/tournament_management', function(req, res, next){
+  res.render('tournament_management', {title: 'Quản lí giải đấu'});
+});
 
 module.exports = router;
